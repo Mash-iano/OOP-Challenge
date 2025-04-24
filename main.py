f161c86aafd0551a8d97fa5f15d2319de501c1bc
Mash-iano <149897328+Mash-iano@users.noreply.github.com>
2025-04-24 22:42:30 +0300

    main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from pet import Pet
 #Create pet
 my_pet = Pet("Bosco")
 
-#some actions
+#actions
 my_pet.get_status()
 my_pet.eat()
 my_pet.sleep()
 my_pet.play()
 my_pet.get_status()
 
-#Bonus
+#The Bonus
 my_pet.train("sit")
 my_pet.train("jump")
 my_pet.show_tricks()
